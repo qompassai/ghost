b8e1c2850c2c0c024cfdab8b9e0de2b72c03a96d
phaedrusflow <map@qompass.ai>
2025-11-25 17:26:32 -0800

    Update 2025-11-25

--- etc/rspamd/lua/rspamd.local.lua
+++
@@ -1,8 +0,0 @@
-rspamd_config.AUTHENTICATED_USER = {
-	callback = function(task)
-		local uname = task:get_user()
-		if uname then
-			return 1
-		end
-	end
-}
--- tools/del_sending_address_from_queue.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#/qompassai/ghost/tools/del_sending_address_from_queue.sh
-# --------------------------------------
-# Copyright (C) 2025 Qompass AI, All rights reserved
-(test "$1" != "") || (echo "Need email address to delete from queue" && exit 1)
-for i in `mailq | grep "$1" | awk '{print $1;}' | sed -e 's/\*//'`; do postsuper -d $i; done
